5bd2284e7e2232348a7342a117e9d0dcd763de01
mprokunin <56938241+mprokunin@users.noreply.github.com>
2022-08-23 10:30:30 +0300

    Update find.bash

--- find.bash
+++ find.bash
@@ -1,2 +1,5 @@
-# Find files older then 5 minutes
+# Find files older then 5 minutes and list
 find /u03/abcd -type f -mmin +5 -exec ls -l {} \;
+
+# Find files older then 234 days and move to subdirectory
+find /u03/abcd -maxdepth 1 -type f -mtime +234 -exec mv {} /u03/abcd/2021/ \;
